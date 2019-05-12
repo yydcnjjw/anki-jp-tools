3767ae5927881b9a79d3872b4e94fcc4a23e6edf
yydcnjjw <yydcnjjw@gmail.com>
2019-05-12 11:29:55 +0800

    Add initial files

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+((nil .
+      ((projectile-project-configure-cmd . "")
+       (projectile-project-compilation-cmd "")
+       (projectile-project-run-cmd "python ")
+       (my/venv . "python3")))
+ (python-mode .
+              ((my/python-path . (""))
+               (my/ld-library-path . ("")))))
