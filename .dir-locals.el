867d10438ab54f59371d71e8418bef6c60854777
yydcnjjw <yydcnjjw@gmail.com>
2019-05-14 17:33:58 +0800

    mv misc file

--- .dir-locals.el
+++
@@ -1,8 +0,0 @@
-((nil .
-      ((projectile-project-configure-cmd . "")
-       (projectile-project-compilation-cmd "")
-       (projectile-project-run-cmd "python ")
-       (my/venv . "python3")))
- (python-mode .
-              ((my/python-path . (""))
-               (my/ld-library-path . ("")))))
